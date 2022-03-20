dfa5c8d8f37885ee1d5d07dffc00e58f0f3c6c59
Connor Turland <connorturland@gmail.com>
2022-03-20 14:55:42 -0700

    initial

---
+++ src/LeftScreenNavMenu.tsx
@@ -0,0 +1,30 @@
+import {
+  SlIcon,
+  SlMenu,
+  SlMenuItem,
+} from "@shoelace-style/shoelace/dist/react";
+import React from "react";
+import { Link } from "react-router-dom";
+
+export type LeftScreenNavMenuProps = {};
+
+const LeftScreenNavMenu: React.FC<LeftScreenNavMenuProps> = ({}) => {
+  return (
+    <div className="left-screen-nav-menu">
+      <SlMenu>
+        <Link to="/resources">
+          <SlMenuItem value="Inventory">
+            <SlIcon slot="prefix" name="minecart-loaded" />
+            Resources
+          </SlMenuItem>
+        </Link>
+        <SlMenuItem value="Agents" disabled>
+          Agents
+          <SlIcon slot="prefix" name="people" />
+        </SlMenuItem>
+      </SlMenu>
+    </div>
+  );
+};
+
+export default LeftScreenNavMenu;
