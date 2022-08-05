3848e5d07c32d1c7c3ee62289742fe3ff43ee6bd
Connor Turland <connor@sprillow.com>
2022-08-05 07:18:35 -0400

    replace cat pic

--- src/Header.tsx
+++ src/Header.tsx
@@ -9,7 +9,7 @@ const Header: React.FC<HeaderProps> = ({ name }) => {
   return (
     <div className="header">
       <SlAvatar
-        image="https://images.unsplash.com/photo-1529778873920-4da4926a72c2?ixlib=rb-1.2.1&auto=format&fit=crop&w=300&q=80"
+        image="https://avatars.githubusercontent.com/u/46187479?s=200&v=4"
         label="Avatar of a gray tabby kitten looking down"
       />
       <div className="search">
